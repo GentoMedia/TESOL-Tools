3048e1bee37ba9e7436c6233a4b9d973cd180cb3
dariensebastion <53331146+GentoMedia@users.noreply.github.com>
2024-07-11 08:14:12 +0900

    Moving Word Search to RichTextLabel

--- Scripts/CEFRJWord.gd
+++ Scripts/CEFRJWord.gd
@@ -4,17 +4,20 @@ extends Control
 var regex = RegEx.new()
 
 @onready var CEFRJWordList = get_node("/root/WordList/")
-@onready var entry = preload("res://Scenes/Entry.tscn")
-@onready var NGSLentry = preload("res://Scenes/NGSLEntry.tscn")
 @onready var credits = preload("res://Scenes/Credits.tscn")
+@onready var NGSLWord = $VBoxContainer/HBoxContainer/NGSLWord
+@onready var CEFRJWord = $VBoxContainer/HBoxContainer/CEFRJWord
+@onready var Data1 = $VBoxContainer/HBoxContainer/Data1
+@onready var Data2 = $VBoxContainer/HBoxContainer/Data2
 
 func _ready():
 	$VBoxContainer/SearchBar/SearchInput.grab_focus()
 
 func _on_SearchButton_pressed(_words : String = "example"):
+	for child in $VBoxContainer/HBoxContainer.get_children():
+		child.clear()
+	
 	regex.compile("[a-z]+")
-	for child in $VBoxContainer/ScrollContainer/WordResults.get_children():
-		child.queue_free()
 	
 	var searchWords = $VBoxContainer/SearchBar/SearchInput.text.split(" ")
 	
@@ -66,15 +69,13 @@ func _on_SearchButton_pressed(_words : String = "example"):
 		
 	searchWords.append_array(tempWords)
 	
-	
+	# *** Change this to update RichTextLabel
 	for word in searchWords:
 		var NGSLentryInt = CEFRJWordList.ngslWords.find(word, 0)
 		if NGSLentryInt >= 0:
-			var newNGSLEntry = NGSLentry.instantiate()
-			$VBoxContainer/ScrollContainer/WordResults.add_child(newNGSLEntry)
-			newNGSLEntry.get_node("HBoxContainer/Word").text = CEFRJWordList.NGSL[NGSLentryInt][0]
-			newNGSLEntry.get_node("HBoxContainer/List").text = CEFRJWordList.NGSL[NGSLentryInt][1]
-			newNGSLEntry.get_node("HBoxContainer/Rank").text = CEFRJWordList.NGSL[NGSLentryInt][2]
+			NGSLWord.append_text("[b]" + CEFRJWordList.NGSL[NGSLentryInt][0] + "[/b]\n")
+			Data1.append_text(CEFRJWordList.NGSL[NGSLentryInt][1] + "\n")
+			Data2.append_text(CEFRJWordList.NGSL[NGSLentryInt][2] + "\n")
 
 		var entries = CEFRJWordList.Words.count(word)
 		var entryPos = 0
@@ -82,10 +83,9 @@ func _on_SearchButton_pressed(_words : String = "example"):
 		while entries > 0:
 			var entryInt = CEFRJWordList.Words.find(word, entryPos)
 			if entryInt >= 0:
-				var newEntry = entry.instantiate()
-				$VBoxContainer/ScrollContainer/WordResults.add_child(newEntry)
-				newEntry.get_node("HBoxContainer/Word").text = CEFRJWordList.EntryList[entryInt][0]
-				newEntry.get_node("HBoxContainer/CEFR").text = CEFRJWordList.EntryList[entryInt][2]
-				newEntry.get_node("HBoxContainer/SpeechPart").text = CEFRJWordList.EntryList[entryInt][1]
+				CEFRJWord.append_text(CEFRJWordList.EntryList[entryInt][0] + "\n")
+				Data1.append_text(CEFRJWordList.EntryList[entryInt][2] + "\n")
+				Data2.append_text(CEFRJWordList.EntryList[entryInt][1] + "\n")
 			entryPos = entryInt + 1
 			entries -= 1
+
